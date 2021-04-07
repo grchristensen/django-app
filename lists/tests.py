7378ec175028b73e98f8fd7b5492d78132ef926e
Gage Christensen <grchr4000@gmail.com>
2021-04-06 17:26:47 -0700

    Fix build stage

--- lists/tests.py
+++ lists/tests.py
@@ -16,6 +16,7 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
+        self.assertTrue(2 == 3)
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
