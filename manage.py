e8cd535e67a90f7fc6ce71ddd78d06ea0fef1f4e
Gage Christensen <grchr4000@gmail.com>
2021-04-06 17:34:08 -0700

    Fix build stage

--- manage.py
+++ manage.py
@@ -7,15 +7,16 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oursite.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        pass
+    # try:
+    #     from django.core.management import execute_from_command_line
+    # except ImportError as exc:
+    #     pass
         # raise ImportError(
         #     "Couldn't import Django. Are you sure it's installed and "
         #     "available on your PYTHONPATH environment variable? Did you "
         #     "forget to activate a virtual environment?"
         # ) from exc
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
 
